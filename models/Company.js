e5e499207e7b2c16a0c19b15bd6b0e6953f2363c
Robert Coster <rob@signalfire.co.uk>
2018-10-02 15:15:27 +0100

    Added user_id field to company schema

--- models/Company.js
+++ models/Company.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const CompanySchema = new Schema({
+  user_id: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
