1f60c5594d9d029a5bab0c5240e813c34c63842c
Robert Coster <rob@signalfire.co.uk>
2018-10-04 16:32:12 +0100

    Added some numbering as I was getting lost...

--- routers/company.js
+++ routers/company.js
@@ -5,35 +5,43 @@ const {getCompanyPlaylists, addCompanyPlaylist, getCompanyPlaylistById, updateCo
 const {getCompanyOffers, getCompanyOfferById, addCompanyOffer, updateCompanyOfferById, deleteCompanyOfferById} = require('../controllers/offers');
 const {addPlaylistTrack, getPlaylistTracks, getPlaylistTrackById, updatePlaylistTrackById, deletePlaylistTrackById} = require('../controllers/tracks');
 
+// 1
 companyRouter.route('/')
     .get(getCompanies)
 
+// 2, 3
 companyRouter.route('/:company_id')
     .get(getCompanyById)
     .put(updateCompanyById);
 
+// 4, 5
 companyRouter.route('/:company_id/playlists')
     .get(getCompanyPlaylists)
     .post(addCompanyPlaylist);
 
+// 6, 7
 companyRouter.route('/:company_id/playlists/:playlist_id/tracks')
     .get(getPlaylistTracks)
     .post(addPlaylistTrack)
 
+// 8, 9, 10
 companyRouter.route('/:company_id/playlists/:playlist_id/tracks/:track_id')
     .get(getPlaylistTrackById)
     .put(updatePlaylistTrackById)
     .delete(deletePlaylistTrackById)
 
+// 11, 12, 13
 companyRouter.route('/:company_id/playlists/:playlist_id')
     .get(getCompanyPlaylistById)
     .put(updateCompanyPlaylistById)
     .delete(deleteCompanyPlaylistById)
 
+// 14, 15
 companyRouter.route('/:company_id/offers')
     .get(getCompanyOffers)
     .post(addCompanyOffer)
 
+// 16, 17, 18
 companyRouter.route('/:company_id/offers/:offer_id')
     .get(getCompanyOfferById)
     .put(updateCompanyOfferById)
